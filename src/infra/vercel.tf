9f1578fa667e27155e4fda445b57b360d176fe34
Calladrus2001 <dugarvishesh@gmail.com>
2026-08-09 14:28:08 +0530

    vercel tf fix

--- src/infra/vercel.tf
+++ src/infra/vercel.tf
@@ -2,14 +2,14 @@ resource "vercel_project_environment_variable" "aws_access_key" {
   project_id = var.vercel_project_id
   key        = "AWS_ACCESS_KEY_ID"
   value      = aws_iam_access_key.s3_user_key.id
-  target     = ["production", "preview", "development"]
-  sensitive  = false
+  target     = ["production", "preview"]
+  sensitive  = true
 }
 
 resource "vercel_project_environment_variable" "aws_secret_key" {
   project_id = var.vercel_project_id
   key        = "AWS_SECRET_ACCESS_KEY"
   value      = aws_iam_access_key.s3_user_key.secret
-  target     = ["production", "preview", "development"]
+  target     = ["production", "preview"]
   sensitive  = true
 }
